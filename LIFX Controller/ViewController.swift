5af4523e9c1558b2faf89ee063b8125373eb59da
Alexander Lester <alexander480@gmail.com>
2020-03-20 23:52:47 -0400

    Initial Commit

---
+++ LIFX Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LIFX Controller
+//
+//  Created by Alexander Lester on 3/20/20.
+//  Copyright © 2020 Alexander Lester. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
