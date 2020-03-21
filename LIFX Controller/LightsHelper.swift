c222b12683478ccef70559ef234b2ff2d6bee65a
Alexander Lester <alexander480@gmail.com>
2020-03-21 01:26:47 -0400

    Integrated LIFXHTTPKit as a Swift Package. Setup for cross-platform

---
+++ LIFX Controller/LightsHelper.swift
@@ -0,0 +1,14 @@
+//
+//  LightsHelper.swift
+//  LIFX Controller
+//
+//  Created by Alexander Lester on 3/21/20.
+//  Copyright © 2020 Alexander Lester. All rights reserved.
+//
+
+import Foundation
+import LIFXHTTPKit
+
+class LightsHelper: NSObject {
+	
+}
